2e5298f73f0b6c6e4f9466066a2b1e643d072e79
Denis Olshin <me@denull.ru>
2019-12-22 01:03:03 +0300

    fixed bidders getter

--- tests/test-new-blind.sh
+++ tests/test-new-blind.sh
@@ -1,2 +1,2 @@
-../new-auction.fif auction $1 $2 64 1577825999 1677825999 10.0 -b 50.0
+../new-auction.fif auction $1 $2 64 1576965453 1677825999 10.0 -b 50.0
 ../../build/lite-client/lite-client  -C ../../ton-lite-client-test1.config.json -c"sendfile auction-query.boc"
